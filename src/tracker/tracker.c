4ce1e8de79dab7c1af3b0822c0347f54ff0a110c
Tanjun <32965926@qq.com>
2019-05-13 08:19:15 +0800

    about telemetry value changed

--- src/tracker/tracker.c
+++ src/tracker/tracker.c
@@ -3,11 +3,16 @@
 #include "protocols/atp.h"
 #include "util/calc.h"
 
-// static const char *TAG = "Tarcker";
+static const char *TAG = "Tarcker";
 static servo_t servo;
+static atp_t atp;
+
+// static Observer telemetry_vals_observer;
 
 static void tracker_status_changed(void *t, tracker_status_e s)
 {
+    LOG_I(TAG, "TRACKER_STATUS_CHANGE -> %d", s);
+
     tracker_t *tracker = t;
     tracker->internal.status = s;
 
@@ -36,13 +41,101 @@ static void tracker_status_changed(void *t, tracker_status_e s)
 static void tracker_flag_changed(void *t, uint8_t f)
 {
     tracker_t *tracker = t;
+    LOG_I(TAG, "TRACKER_FLAG_CHANGED -> %d | %d = %d", tracker->internal.flag, f, tracker->internal.flag | f);
+
     tracker->internal.flag |= f;
+    time_micros_t now = time_micros_now();
 
     // #if defined(USE_BEEPER)
     //     if (f & (TRACKER_FLAG_HOMESETED | TRACKER_FLAG_PLANESETED)) beeper_set_mode(&t->ui.internal.beeper, BEEPER_MODE_SETED);
     // #endif
-
+    
+    ATP_SET_U8(TAG_TRACKER_FLAG, tracker->internal.flag, now);
     tracker->internal.flag_changed_notifier->mSubject.Notify(tracker->internal.flag_changed_notifier, &f);
+}
+
+static void tracker_telemetry_changed(void *t, uint8_t tag)
+{
+    tracker_t *tracker = (tracker_t *)t;
+
+    switch (tag)
+    {
+    case TAG_BASE_ACK:
+        if (!(telemetry_get_u8(atp_get_tag_val(TAG_TRACKER_FLAG)) & TRACKER_FLAG_SERVER_CONNECTED))
+        {   
+            tracker->internal.flag_changed(tracker, TRACKER_FLAG_SERVER_CONNECTED);
+        }
+        tracker->last_heartbeat = time_millis_now();
+        break;
+    case TAG_PLANE_LONGITUDE:
+        if (!(tracker->internal.flag & TRACKER_FLAG_PLANESETED))
+            tracker->internal.flag_changed(tracker, TRACKER_FLAG_PLANESETED);
+        break;
+    case TAG_PLANE_LATITUDE:
+        if (!(tracker->internal.flag & TRACKER_FLAG_PLANESETED))
+            tracker->internal.flag_changed(tracker, TRACKER_FLAG_PLANESETED);
+        break;
+    case TAG_TRACKER_LONGITUDE:
+        if (!(tracker->internal.flag & TRACKER_FLAG_HOMESETED))
+            tracker->internal.flag_changed(tracker, TRACKER_FLAG_HOMESETED);
+    case TAG_TRACKER_LATITUDE:
+        if (!(tracker->internal.flag & TRACKER_FLAG_HOMESETED))
+            tracker->internal.flag_changed(tracker, TRACKER_FLAG_HOMESETED);
+        break;
+    case TAG_TRACKER_MODE:
+        tracker->internal.status = telemetry_get_u8(atp_get_tag_val(tag));
+        break;
+    case TAG_TRACKER_FLAG:
+        break;
+    default:
+        break;
+    }
+}
+
+static bool tracker_check_atp_cmd(tracker_t *t)
+{
+    if (!(t->internal.flag & TRACKER_FLAG_WIFI_CONNECTED))
+        return false;
+
+    time_millis_t now = time_millis_now(); 
+
+    // if (t->last_heartbeat + 30000 < now && t->internal.flag & TRACKER_FLAG_SERVER_CONNECTED)
+    // {
+    //     t->internal.status_changed(t, TRACKER_STATUS_SERVER_CONNECTING);
+    //     return false;
+    // }
+
+    if (!(t->internal.flag & TRACKER_FLAG_SERVER_CONNECTED))
+    {
+        
+        if (now > t->last_heartbeat + 2000)
+        {
+            t->atp->enc_frame->atp_cmd = CMD_HEARTBEAT;
+            uint8_t *buff = atp_frame_encode(t->atp->enc_frame);
+            if (t->atp->enc_frame->buffer_index > 0)
+            {
+                t->atp->atp_send(buff, t->atp->enc_frame->buffer_index);
+                t->last_heartbeat = now;
+                return true;
+            }
+        }
+    }
+    else
+    {
+        if (now > t->last_heartbeat + 10000)
+        {
+            t->atp->enc_frame->atp_cmd = CMD_HEARTBEAT;
+            uint8_t *buff = atp_frame_encode(t->atp->enc_frame);
+            if (t->atp->enc_frame->buffer_index > 0)
+            {
+                t->atp->atp_send(buff, t->atp->enc_frame->buffer_index);
+                t->last_heartbeat = now;
+                return true;
+            }
+        }
+    }
+
+    return false;
 }
 
 void tracker_init(tracker_t *t)
@@ -60,54 +153,68 @@ void tracker_init(tracker_t *t)
     t->internal.status_changed_notifier = (notifier_t *)Notifier_Create(sizeof(notifier_t));
     t->internal.status_changed = tracker_status_changed;
     t->internal.flag_changed = tracker_flag_changed;
+    t->internal.telemetry_changed = tracker_telemetry_changed;
     t->internal.status_changed(t, TRACKER_STATUS_BOOTING);
-    t->servo = &servo;
+    t->last_heartbeat = time_millis_now();
 
-    // servo_init(&servo, t->ui);
+    t->servo = &servo;
+    t->atp = &atp;
+    t->atp->tracker = t;
+    t->atp->tag_value_changed = t->internal.telemetry_changed;
+    
+    // atp.flag_change = tracker_flag_changed;
+    // atp.telemetry_val_notifier = (notifier_t *)Notifier_Create(sizeof(notifier_t));
+    // telemetry_vals_observer.Obj = t;
+    // telemetry_vals_observer.Name = "Telemetry values observer";
+    // telemetry_vals_observer.Update = telemetry_vals_updated;
+    // atp.telemetry_val_notifier->mSubject.Attach(atp.telemetry_val_notifier, &telemetry_vals_observer);
+
     servo_init(&servo);
-    atp_init(t);
+    atp_init(&atp);
 }
 
 void task_tracker(void *arg)
 {
-    tracker_t *tracker = arg;
+    tracker_t *t = arg;
 
     time_ticks_t sleep;
 
-    servo_pulsewidth_out(&servo.internal.pan, servo.internal.pan.config.max_pulsewidth);
-    servo_pulsewidth_out(&servo.internal.tilt, servo.internal.pan.config.max_pulsewidth);
-    vTaskDelay(MILLIS_TO_TICKS(3000));
+    // servo_pulsewidth_out(&servo.internal.pan, servo.internal.pan.config.max_pulsewidth);
+    // servo_pulsewidth_out(&servo.internal.tilt, servo.internal.pan.config.max_pulsewidth);
     servo_pulsewidth_out(&servo.internal.pan, servo.internal.pan.config.min_pulsewidth);
     servo_pulsewidth_out(&servo.internal.tilt, servo.internal.pan.config.min_pulsewidth);
+
+    vTaskDelay(MILLIS_TO_TICKS(3000));
 
     uint16_t distance = 0;
 
     while (1)
     {
-        if (tracker->internal.mode == TRACKER_MODE_AUTO)
+        if (t->internal.mode == TRACKER_MODE_AUTO)
         {
-            if (tracker->internal.flag & (TRACKER_FLAG_HOMESETED | TRACKER_FLAG_PLANESETED))
+            if (t->internal.flag & (TRACKER_FLAG_HOMESETED | TRACKER_FLAG_PLANESETED))
             {
                 //pan
                 if (TIME_CYCLE_EVERY_MS(150, 2) == 0)
                 {
-                    float tracker_lat = telemetry_get_i32(atp_get_tag_val(TAG_HOME_LATITUDE)) / 10000000.0f;
-                    float tracker_lon = telemetry_get_i32(atp_get_tag_val(TAG_HOME_LONGITUDE)) / 10000000.0f;
+                    float tracker_lat = telemetry_get_i32(atp_get_tag_val(TAG_TRACKER_LATITUDE)) / 10000000.0f;
+                    float tracker_lon = telemetry_get_i32(atp_get_tag_val(TAG_TRACKER_LONGITUDE)) / 10000000.0f;
                     float plane_lat = telemetry_get_i32(atp_get_tag_val(TAG_PLANE_LATITUDE)) / 10000000.0f;
                     float plane_lon = telemetry_get_i32(atp_get_tag_val(TAG_PLANE_LONGITUDE)) / 10000000.0f;
 
                     distance = distance_between(tracker_lat, tracker_lon, plane_lat, plane_lon);
-
-                    servo.internal.pan.currtent_degree = course_to(tracker_lat, tracker_lon, plane_lat, plane_lon) * 10.0f;
+                    // printf("tracker_lat:%f | tracker_lon:%f | plane_lat:%f | plane_lon:%f | distance:%d\n", tracker_lat, tracker_lon, plane_lat, plane_lon, distance);
+                    servo.internal.pan.currtent_degree = course_to(tracker_lat, tracker_lon, plane_lat, plane_lon);
                     servo_pulsewidth_control(&servo.internal.pan, &servo.internal.ease_config);
                 }
 
                 //tilt
                 if (TIME_CYCLE_EVERY_MS(200, 2) == 0)
                 {
-                    int16_t tracker_alt = telemetry_get_i16(atp_get_tag_val(TAG_HOME_ALTITUDE));
-                    int16_t plane_alt = telemetry_get_i16(atp_get_tag_val(TAG_HOME_ALTITUDE));
+                    int32_t tracker_alt = telemetry_get_i32(atp_get_tag_val(TAG_TRACKER_ALTITUDE));
+                    int32_t plane_alt = telemetry_get_i32(atp_get_tag_val(TAG_PLANE_ALTITUDE));
 
+                    // printf("tracker_alt:%d | plane_alt:%d | distance:%d\n", tracker_alt, plane_alt, distance);
                     servo.internal.tilt.currtent_degree = tilt_to(distance, tracker_alt, plane_alt);
                     servo_pulsewidth_control(&servo.internal.tilt, &servo.internal.ease_config);
                 }
@@ -115,7 +222,7 @@ void task_tracker(void *arg)
                 servo_reverse_check(&servo);
             }
         }
-        else if (tracker->internal.mode == TRACKER_MODE_MANUAL)
+        else if (t->internal.mode == TRACKER_MODE_MANUAL)
         {
             servo_update(&servo);
         }
@@ -130,22 +237,11 @@ void task_tracker(void *arg)
             sleep = MILLIS_TO_TICKS(50);
         }
 
-        vTaskDelay(sleep);
+        if (!tracker_check_atp_cmd(t))
+        {
+            vTaskDelay(sleep);
+        }
     }
-}
-
-const char *telemetry_format_tracker_mode(const telemetry_t *val, char *buf, size_t bufsize)
-{
-    switch ((tracker_mode_e)val->val.u8)
-    {
-    case TRACKER_MODE_AUTO:
-        return "Tracking";
-    case TRACKER_MODE_MANUAL:
-        return "Manual";
-    case TRACKER_MODE_DEBUG:
-        return "Debug";
-    }
-    return NULL;
 }
 
 tracker_status_e get_tracker_status(const tracker_t *t)
--- src/tracker/tracker.h
+++ src/tracker/tracker.h
@@ -4,12 +4,14 @@
 #include "telemetry.h"
 #include "servo.h"
 #include "observer.h"
+// #include "protocols/atp.h"
 
 // #if defined(USE_WIFI)
 // #include "wifi/wifi.h"
 // #endif
 
 typedef struct _Notifier notifier_t;
+typedef struct atp_s atp_t;
 
 typedef enum
 {
@@ -28,8 +30,9 @@ typedef enum
     TRACKER_FLAG_PLANESETED = 2,
     TRACKER_FLAG_TRACKING = 4,
     TRACKER_FLAG_AUTO_NORTH = 8,
+    TRACKER_FLAG_WIFI_CONNECTED = 16,
+    TRACKER_FLAG_SERVER_CONNECTED = 32,
 } tracker_flag_e;
-
 
 typedef enum
 {
@@ -38,17 +41,13 @@ typedef enum
     TRACKER_MODE_DEBUG,
 } tracker_mode_e;
 
-typedef void (*pTr_atp_decode)(void *buffer, int offset, int len);
 typedef void (*pTr_status_changed)(void *t, tracker_status_e s);
 typedef void (*pTr_flag_changed)(void *t, uint8_t f);
+typedef void (*pTr_telemetry_changed)(void *t, uint8_t tag);
 
 typedef struct tracker_s
 {
     time_millis_t last_heartbeat;
-
-    telemetry_t *plane_tags;
-    telemetry_t *home_tags;
-    telemetry_t *param_tags;
 
     struct
     {
@@ -57,14 +56,13 @@ typedef struct tracker_s
         tracker_mode_e mode;
         pTr_status_changed status_changed;
         pTr_flag_changed flag_changed;
-        pTr_atp_decode atp_decode;
+        pTr_telemetry_changed telemetry_changed;
         notifier_t *status_changed_notifier;
         notifier_t *flag_changed_notifier;
     } internal;
 
     servo_t *servo;
-    // wifi_t *wifi;
-    // ui_t *ui;
+    atp_t *atp;
 } tracker_t;
 
 void tracker_init(tracker_t *t);
