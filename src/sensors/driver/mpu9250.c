c03f1881318fd9c214490fa10d5ccf6740939c71
TanJun <32965926@qq.com>
2020-05-25 10:54:20 +0800

    mpu9250 driver.

---
+++ src/sensors/driver/mpu9250.c
@@ -0,0 +1,253 @@
+#include <hal/log.h>
+#include "mpu9250.h"
+#include "mpu9250_i2c.h"
+#include "sdkconfig.h"
+
+const static char *TAG = "mpu9250";
+
+static const float _accel_lsbs[] =
+    {
+        1.0f / MPU9250_ACCE_SENS_2,
+        1.0f / MPU9250_ACCE_SENS_4,
+        1.0f / MPU9250_ACCE_SENS_8,
+        1.0f / MPU9250_ACCE_SENS_16,
+};
+
+static const float _gyro_lsbs[] =
+    {
+        1.0f / MPU9250_GYRO_SENS_250,
+        1.0f / MPU9250_GYRO_SENS_500,
+        1.0f / MPU9250_GYRO_SENS_1000,
+        1.0f / MPU9250_GYRO_SENS_2000,
+};
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// private utilities
+//
+////////////////////////////////////////////////////////////////////////////////
+static inline void mpu9250_write_reg(mpu9250_t *mpu9250, uint8_t reg, uint8_t data)
+{
+  uint8_t buffer[2];
+
+  buffer[0] = reg;
+  buffer[1] = data;
+
+  if (mpu9250_i2c_write(&mpu9250->mpu9250_i2c_cfg, buffer, 2) == false)
+  {
+    LOG_E(TAG, "mpu9250_write_reg: failed to mpu9250_i2c_write");
+  }
+}
+
+static inline void mpu9250_write_reg16(mpu9250_t *mpu9250, uint8_t reg, uint16_t data)
+{
+  uint8_t buffer[3];
+
+  buffer[0] = reg;
+  buffer[1] = (data >> 8) & 0xff;
+  buffer[2] = data & 0xff;
+
+  if (mpu9250_i2c_write(&mpu9250->mpu9250_i2c_cfg, buffer, 3) == false)
+  {
+    LOG_E(TAG, "mpu9250_write_reg16: failed to mpu9250_i2c_write");
+  }
+}
+
+static inline uint8_t mpu9250_read_reg(mpu9250_t *mpu9250, uint8_t reg)
+{
+  uint8_t ret;
+
+  if (mpu9250_i2c_write(&mpu9250->mpu9250_i2c_cfg, &reg, 1) == false)
+  {
+    LOG_E(TAG, "mpu9250_read_reg: failed to mpu9250_i2c_write");
+  }
+
+  if (mpu9250_i2c_read(&mpu9250->mpu9250_i2c_cfg, &ret, 1) == false)
+  {
+    LOG_E(TAG, "mpu9250_read_reg: failed to mpu9250_i2c_read");
+  }
+
+  return ret;
+}
+
+static inline void mpu9250_read_data(mpu9250_t *mpu9250, uint8_t reg, uint8_t *data, uint8_t len)
+{
+  if (mpu9250_i2c_write(&mpu9250->mpu9250_i2c_cfg, &reg, 1) == false)
+  {
+    LOG_E(TAG, "mpu9250_read_data: failed to mpu9250_i2c_write");
+  }
+
+  if (mpu9250_i2c_read(&mpu9250->mpu9250_i2c_cfg, data, len) == false)
+  {
+    LOG_E(TAG, "mpu9250_read_data: failed to mpu9250_i2c_read");
+  }
+}
+
+static inline uint8_t ak8963_read_reg(mpu9250_t *mpu9250, uint8_t reg)
+{
+  uint8_t ret;
+
+  if (ak8963_i2c_write(&mpu9250->mpu9250_i2c_cfg, &reg, 1) == false)
+  {
+    LOG_E(TAG, "ak8963_read_reg: failed to mpu9250_i2c_write");
+  }
+
+  if (ak8963_i2c_read(&mpu9250->mpu9250_i2c_cfg, &ret, 1) == false)
+  {
+    LOG_E(TAG, "ak8963_read_reg: failed to mpu9250_i2c_read");
+  }
+  return ret;
+}
+
+static inline void ak8963_write_reg(mpu9250_t *mpu9250, uint8_t reg, uint8_t data)
+{
+  uint8_t buffer[2];
+
+  buffer[0] = reg;
+  buffer[1] = data;
+
+  if (ak8963_i2c_write(&mpu9250->mpu9250_i2c_cfg, buffer, 2) == false)
+  {
+    LOG_E(TAG, "ak8963_write_reg: failed to mpu9250_i2c_write");
+  }
+}
+
+static inline void ak8963_read_data(mpu9250_t *mpu9250, uint8_t reg, uint8_t *data, uint8_t len)
+{
+  if (ak8963_i2c_write(&mpu9250->mpu9250_i2c_cfg, &reg, 1) == false)
+  {
+    LOG_E(TAG, "ak8963_read_data: failed to mpu9250_i2c_write");
+  }
+
+  if (ak8963_i2c_read(&mpu9250->mpu9250_i2c_cfg, data, len) == false)
+  {
+    LOG_E(TAG, "ak8963_read_data: failed to mpu9250_i2c_read");
+  }
+}
+
+static void ak8963_init(mpu9250_t *mpu9250)
+{
+  uint8_t v;
+
+  v = ak8963_read_reg(mpu9250, AK8963_WIA);
+  ESP_LOGI(TAG, "ak8963 chip ID: %x", v);
+
+  // put ak8963 in mode 2 for 100Hz measurement
+  // also set 16 bit output
+  v = ((1 << 4) | 0x06);
+  ak8963_write_reg(mpu9250, AK8963_CNTL1, v);
+
+  v = ak8963_read_reg(mpu9250, AK8963_CNTL1);
+  ESP_LOGI(TAG, "ak8963 control reg: %x", v);
+}
+
+static void ak8963_read_all(mpu9250_t *mpu9250, imu_sensor_data_t *imu)
+{
+  uint8_t data[7];
+
+  ak8963_read_data(mpu9250, AK8963_HXL, data, 7);
+
+  imu->mag[0] = (int16_t)(data[1] << 8 | data[0]);
+  imu->mag[1] = (int16_t)(data[3] << 8 | data[2]);
+  imu->mag[2] = (int16_t)(data[5] << 8 | data[4]);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// public utilities
+//
+////////////////////////////////////////////////////////////////////////////////
+void mpu9250_init(mpu9250_t *mpu9250,
+                  MPU9250_Accelerometer_t accel_sensitivity,
+                  MPU9250_Gyroscope_t gyro_sensitivity,
+                  imu_raw_to_real_t *lsb)
+{
+  mpu9250_i2c_init(&mpu9250->mpu9250_i2c_cfg);
+
+  mpu9250->accel_config = accel_sensitivity;
+  mpu9250->gyro_config = gyro_sensitivity;
+
+  uint8_t temp;
+  /* Wakeup MPU6050 */
+  mpu9250_write_reg(mpu9250, MPU9250_PWR_MGMT_1, 0x00);
+  /* Config accelerometer */
+  temp = mpu9250_read_reg(mpu9250, MPU9250_ACCEL_CONFIG);
+  temp = (temp & 0xE7) | (uint8_t)accel_sensitivity << 3;
+  mpu9250_write_reg(mpu9250, MPU9250_ACCEL_CONFIG, temp);
+  /* Config gyroscope */
+  temp = mpu9250_read_reg(mpu9250, MPU9250_GYRO_CONFIG);
+  temp = (temp & 0xE7) | (uint8_t)gyro_sensitivity << 3;
+  mpu9250_write_reg(mpu9250, MPU9250_GYRO_CONFIG, temp);
+  // enable bypass mode to access AK8963
+  mpu9250_write_reg(mpu9250, 55, 0x02);
+  ak8963_init(mpu9250);
+
+  lsb->accel_lsb = _accel_lsbs[accel_sensitivity];
+  lsb->gyro_lsb = _gyro_lsbs[gyro_sensitivity];
+  lsb->mag_lsb = AK8963_MAG_LSB;
+}
+
+bool mpu9250_read_gyro_accel(mpu9250_t *mpu9250, imu_sensor_data_t *imu)
+{
+  uint8_t data[14];
+
+  // read full raw data
+  mpu9250_read_data(mpu9250, MPU9250_ACCEL_XOUT_H, data, 14);
+  //printf("[0][%d]    [1][%d]\r\n", data[0], data[1]);
+  imu->accel[0] = (int16_t)(data[0] << 8 | data[1]);
+  imu->accel[1] = (int16_t)(data[2] << 8 | data[3]);
+  imu->accel[2] = (int16_t)(data[4] << 8 | data[5]);
+
+  imu->temp = (data[6] << 8 | data[7]);
+
+  imu->gyro[0] = (int16_t)(data[8] << 8 | data[9]);
+  imu->gyro[1] = (int16_t)(data[10] << 8 | data[11]);
+  imu->gyro[2] = (int16_t)(data[12] << 8 | data[13]);
+
+  return true;
+}
+
+bool mpu9250_read_mag(mpu9250_t *mpu9250, imu_sensor_data_t *imu)
+{
+  ak8963_read_all(mpu9250, imu);
+  return true;
+}
+
+bool mpu9250_read_all(mpu9250_t *mpu9250, imu_sensor_data_t *imu)
+{
+  uint8_t data[14];
+
+  // read full raw data
+  mpu9250_read_data(mpu9250, MPU9250_ACCEL_XOUT_H, data, 14);
+
+  imu->accel[0] = (int16_t)(data[0] << 8 | data[1]);
+  imu->accel[1] = (int16_t)(data[2] << 8 | data[3]);
+  imu->accel[2] = (int16_t)(data[4] << 8 | data[5]);
+
+  imu->temp = (data[6] << 8 | data[7]);
+
+  imu->gyro[0] = (int16_t)(data[8] << 8 | data[9]);
+  imu->gyro[1] = (int16_t)(data[10] << 8 | data[11]);
+  imu->gyro[2] = (int16_t)(data[12] << 8 | data[13]);
+
+  ak8963_read_all(mpu9250, imu);
+
+  return true;
+}
+
+/*
+void
+mpu9250_convert_to_eng_units(mpu9250_t* mpu9250, imu_sensor_data_t* imu)
+{
+  imu->accel[0] = imu->accel_raw[0] * mpu9250->accel_lsb;
+  imu->accel[1] = imu->accel_raw[1] * mpu9250->accel_lsb;
+  imu->accel[2] = imu->accel_raw[2] * mpu9250->accel_lsb;
+  imu->temp = (imu->temp_raw / 340333.87f + 21.0f);
+  imu->gyro[0]  = imu->gyro_raw[0] * mpu9250->gyro_lsb;
+  imu->gyro[1]  = imu->gyro_raw[1] * mpu9250->gyro_lsb;
+  imu->gyro[2]  = imu->gyro_raw[2] * mpu9250->gyro_lsb;
+  imu->mag[0] = imu->mag_raw[0] * mpu9250->mag_lsb;
+  imu->mag[1] = imu->mag_raw[1] * mpu9250->mag_lsb;
+  imu->mag[2] = imu->mag_raw[2] * mpu9250->mag_lsb;
+}
+ */
---
+++ src/sensors/driver/mpu9250.h
@@ -0,0 +1,136 @@
+#ifndef __MPU_9250_DEF_H__
+#define __MPU_9250_DEF_H__
+
+#include "../imu.h"
+#include "mpu9250_i2c.h"
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// XXX refer to page 38 of MPU9250 Product specification
+// for accel/gyro/mag orientation
+//
+//
+//      chip orientation mark: top left
+//
+//      accel/gyro
+//      X : left positive, counter clock
+//      y : forward positive, counter clock
+//      Z : up positive, counter clock
+//
+//      mag
+//      x : forward positive
+//      y : left positive
+//      z : down positive
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#define MPU9250_I2C_ADDR        0x68      // or 110 100x. 400Khz interface
+
+/* MPU9250 registers */
+#define MPU9250_AUX_VDDIO             0x01
+#define MPU9250_SMPLRT_DIV            0x19
+#define MPU9250_REG_ACCEL_XOFFS_H     (0x06)
+#define MPU9250_REG_ACCEL_XOFFS_L     (0x07)
+#define MPU9250_REG_ACCEL_YOFFS_H     (0x08)
+#define MPU9250_REG_ACCEL_YOFFS_L     (0x09)
+#define MPU9250_REG_ACCEL_ZOFFS_H     (0x0A)
+#define MPU9250_REG_ACCEL_ZOFFS_L     (0x0B)
+#define MPU9250_REG_GYRO_XOFFS_H      (0x13)
+#define MPU9250_REG_GYRO_XOFFS_L      (0x14)
+#define MPU9250_REG_GYRO_YOFFS_H      (0x15)
+#define MPU9250_REG_GYRO_YOFFS_L      (0x16)
+#define MPU9250_REG_GYRO_ZOFFS_H      (0x17)
+#define MPU9250_REG_GYRO_ZOFFS_L      (0x18)
+#define MPU9250_CONFIG                0x1A
+#define MPU9250_GYRO_CONFIG           0x1B
+#define MPU9250_ACCEL_CONFIG          0x1C
+#define MPU9250_MOTION_THRESH         0x1F
+#define MPU9250_INT_PIN_CFG           0x37
+#define MPU9250_INT_ENABLE            0x38
+#define MPU9250_INT_STATUS            0x3A
+#define MPU9250_ACCEL_XOUT_H          0x3B
+#define MPU9250_ACCEL_XOUT_L          0x3C
+#define MPU9250_ACCEL_YOUT_H          0x3D
+#define MPU9250_ACCEL_YOUT_L          0x3E
+#define MPU9250_ACCEL_ZOUT_H          0x3F
+#define MPU9250_ACCEL_ZOUT_L          0x40
+#define MPU9250_TEMP_OUT_H            0x41
+#define MPU9250_TEMP_OUT_L            0x42
+#define MPU9250_GYRO_XOUT_H           0x43
+#define MPU9250_GYRO_XOUT_L           0x44
+#define MPU9250_GYRO_YOUT_H           0x45
+#define MPU9250_GYRO_YOUT_L           0x46
+#define MPU9250_GYRO_ZOUT_H           0x47
+#define MPU9250_GYRO_ZOUT_L           0x48
+#define MPU9250_MOT_DETECT_STATUS     0x61
+#define MPU9250_SIGNAL_PATH_RESET     0x68
+#define MPU9250_MOT_DETECT_CTRL       0x69
+#define MPU9250_USER_CTRL             0x6A
+#define MPU9250_PWR_MGMT_1            0x6B
+#define MPU9250_PWR_MGMT_2            0x6C
+#define MPU9250_FIFO_COUNTH           0x72
+#define MPU9250_FIFO_COUNTL           0x73
+#define MPU9250_FIFO_R_W              0x74
+#define MPU9250_WHO_AM_I              0x75
+    
+/* Gyro sensitivities in °/s */
+#define MPU9250_GYRO_SENS_250       ((float) 131)
+#define MPU9250_GYRO_SENS_500       ((float) 65.5)
+#define MPU9250_GYRO_SENS_1000      ((float) 32.8)
+#define MPU9250_GYRO_SENS_2000      ((float) 16.4)
+    
+/* Acce sensitivities in g */
+#define MPU9250_ACCE_SENS_2         ((float) 16384)
+#define MPU9250_ACCE_SENS_4         ((float) 8192)
+#define MPU9250_ACCE_SENS_8         ((float) 4096)
+#define MPU9250_ACCE_SENS_16        ((float) 2048)
+
+#define AK8963_WIA                  0x00
+#define AK8963_INFO                 0x01
+#define AK8963_ST1                  0x02
+#define AK8963_HXL                  0x03
+#define AK8963_HXH                  0x04
+#define AK8963_HYL                  0x05
+#define AK8963_HYH                  0x06
+#define AK8963_HZL                  0x07
+#define AK8963_HZH                  0x08
+#define AK8963_ST2                  0x09
+#define AK8963_CNTL1                0x0a
+#define AK8963_ASTC                 0x0c
+
+#define AK8963_I2C_ADDR             0x0c
+#define AK8963_MAG_LSB              0.15f     // in 16 bit mode, 0.15uT per LSB
+
+typedef enum
+{   
+  MPU9250_Accelerometer_2G  = 0x00, /*!< Range is +- 2G */
+  MPU9250_Accelerometer_4G  = 0x01, /*!< Range is +- 4G */
+  MPU9250_Accelerometer_8G  = 0x02, /*!< Range is +- 8G */
+  MPU9250_Accelerometer_16G = 0x03  /*!< Range is +- 16G */
+} MPU9250_Accelerometer_t;
+
+typedef enum 
+{ 
+  MPU9250_Gyroscope_250s  = 0x00,   /*!< Range is +- 250 degrees/s */
+  MPU9250_Gyroscope_500s  = 0x01,   /*!< Range is +- 500 degrees/s */
+  MPU9250_Gyroscope_1000s = 0x02,   /*!< Range is +- 1000 degrees/s */
+  MPU9250_Gyroscope_2000s = 0x03    /*!< Range is +- 2000 degrees/s */
+} MPU9250_Gyroscope_t; 
+
+typedef struct
+{
+    mpu9250_i2c_config_t mpu9250_i2c_cfg;
+    MPU9250_Accelerometer_t   accel_config;
+    MPU9250_Gyroscope_t       gyro_config;
+} mpu9250_t;
+
+extern void mpu9250_init(mpu9250_t* mpu9250,
+    MPU9250_Accelerometer_t accel_sensitivity,
+    MPU9250_Gyroscope_t gyro_sensitivity,
+    imu_raw_to_real_t* lsb);
+
+extern bool mpu9250_read_all(mpu9250_t* mpu9250, imu_sensor_data_t* data);
+extern bool mpu9250_read_mag(mpu9250_t* mpu9250, imu_sensor_data_t* imu);
+extern bool mpu9250_read_gyro_accel(mpu9250_t* mpu9250, imu_sensor_data_t* imu);
+
+#endif /* !__MPU_9250_DEF_H__ */
