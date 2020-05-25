11f9ed6dc060e6219f019b5acbde907be2980dae
TanJun <32965926@qq.com>
2020-05-25 10:53:56 +0800

    abstract i2c layer.

--- src/io/hal_i2c.c
+++ src/io/hal_i2c.c
@@ -4,11 +4,26 @@
 
 #include "hal_i2c.h"
 #include <hal/err.h>
+#include "freertos/semphr.h"
+
+static SemaphoreHandle_t _mutexs[2];
 
 void hal_i2c_init(hal_i2c_config_t *cfg)
 {
+    HAL_ERR_ASSERT_OK(hal_i2c_bus_init(cfg->i2c_bus, cfg->sda, cfg->scl, cfg->freq_hz));
 
-    HAL_ERR_ASSERT_OK(hal_i2c_bus_init(cfg->i2c_bus, cfg->sda, cfg->scl, I2C_MASTER_FREQ_HZ));
+    _mutexs[0] = xSemaphoreCreateMutex();
+    _mutexs[1] = xSemaphoreCreateMutex();
 
     cfg->is_init = 1;
+}
+
+void hal_i2c_cmd_take(hal_i2c_bus_t i2c_bus)
+{
+    xSemaphoreTake(_mutexs[i2c_bus], portMAX_DELAY);
+}
+
+void hal_i2c_cmd_give(hal_i2c_bus_t i2c_bus)
+{
+    xSemaphoreGive(_mutexs[i2c_bus]);
 }
