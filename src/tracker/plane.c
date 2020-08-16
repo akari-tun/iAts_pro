5ff7630ced240f75a4e664ae633d55c04182b666
TanJun <32965926@qq.com>
2020-08-16 14:58:34 +0800

    fix altitude error.

--- src/tracker/plane.c
+++ src/tracker/plane.c
@@ -12,5 +12,5 @@ void plane_update(plane_t *plane)
 {
     plane->latitude = telemetry_get_i32(atp_get_telemetry_tag_val(TAG_PLANE_LATITUDE)) /  10000000.0f;
     plane->longitude = telemetry_get_i32(atp_get_telemetry_tag_val(TAG_PLANE_LONGITUDE)) /  10000000.0f;
-    plane->altitude = telemetry_get_i32(atp_get_telemetry_tag_val(TAG_TRACKER_ALTITUDE));
+    plane->altitude = telemetry_get_i32(atp_get_telemetry_tag_val(TAG_PLANE_ALTITUDE));
 }
